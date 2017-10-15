244a4a6c3d74734f48503b93706afca23ede4272
Peter McGill <petermcgill@Peters-MacBook-Pro.local>
2017-10-15 18:10:27 +0100

    made some progress on determingin the lens box, need to decide on convention to return x and y values, pairs or lists

--- lens.py
+++ lens.py
@@ -11,7 +11,7 @@ class Lens:
 	GAIA_END = '2019-01-01'
  
         #min angular with for lens to pass in front of source [Arcseconds]
-	min_angular_width = 0.7  
+	min_angular_width = 0.0007  
 
 	#mas to degree conversion
 	mas_to_deg = (1.0 / 3600)*10**(-6)
@@ -73,16 +73,26 @@ class Lens:
 		start= self.get_eq_coords_at_epoch(self.datetime_to_jyTCB(self.GAIA_START))
 		end = self.get_eq_coords_at_epoch(self.datetime_to_jyTCB(self.GAIA_END))
 		return [[start[0],end[0]],[start[1],end[1]]]	
-		
-		
+
+	def get_lens_box(self):
+		start_end = self.get_GAIA_start_end_coords()
+		deltaX = self.min_angular_width * (start_end[1][0] - start_end[1][1]) / np.sqrt((start_end[0][0] - start_end[0][1])**(2) + (start_end[1][0] - start_end[1][1])**(2))
+		deltaY = self.min_angular_width * (start_end[0][1] - start_end[0][0]) / np.sqrt((start_end[0][0] - start_end[0][1])**(2) + (start_end[1][0] - start_end[1][1])**(2))		
+		print(deltaX)
+		print(deltaY)
+		return [[start_end[0][0] + deltaX,start_end[0][0] - deltaX,start_end[0][1] + deltaX,start_end[0][1] - deltaX],[start_end[1][0] + deltaY,start_end[1][0] - deltaY,start_end[1][1] + deltaY,start_end[1][1] - deltaY]]
+
 lens1 = Lens(12,1,2,1500000,1500000,2012.0)
 
 
 ans = lens1.getId()
 x = lens1.get_GAIA_start_end_coords()
 init = lens1.get_eq_coords_at_epoch(2012.0)
+box = lens1.get_lens_box()
 plt.scatter(x[0],x[1])
 plt.scatter(init[0],init[1])
+plt.scatter(box[0],box[1])
+plt.plot(box[0],box[1])
 plt.text(1.996, 1.01, r'Lens with $\delta_{0}=1,\alpha_{0}=2,\mu_{\delta}=\mu_{\alpha}=150$[mas/yr]')
 plt.ylabel(r'$\delta$')
 plt.xlabel(r'$\alpha\cos\delta$')
