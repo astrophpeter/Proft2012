30865050a96ac2998f279ae9f4de2809f56cf627
Peter McGill <petermcgill@dhcp-10-249-190-120.eduroam.wireless.private.cam.ac.uk>
2017-11-07 16:08:59 +0000

    added more doc strings

--- lens.py
+++ lens.py
@@ -47,16 +47,18 @@ class Lens:
 			epoch_0 (double) : Reference epoch in Julian year 
                                            with Barycentric coordinate time (TCB)
 		"""
-		
+		#set all properties for lens
 		self._id = id
 		self._ra_0 = ra_0
 		self._dec_0 = dec_0
 		self._pmra = pmra
 		self._pmdec = pmdec
 		self._epoch_0 = epoch_0
+		
+		#calulate the search window for this lens
+		#done on initialisation to speed things up.
 		self._lens_box = self.get_lens_box()
 
-                #Time(epoch_0,scale=scale_in,format=format_in)
         
 	def getId(self):
 		"""
@@ -106,13 +108,20 @@ class Lens:
 						   [ra*cos(dec),dec][degrees]
 
 		"""
-		#ra = self._ra_0 + (epoch - self._epoch_0) * self._pmra * self.mas_to_deg
+		
+		
+                #compute ra*cos(dec) at time epoch 
+		#This is where I suspect the issues lie - had a dicussion with Leigh about
+		# this and we agreed it was fine however.
+		#note self._pmra already includes the cos(dec) factor. 
 		raCosDec = (self._ra_0*np.cos(np.deg2rad(self._dec_0))) + (epoch - self._epoch_0)* self._pmra * self.mas_to_deg
+		
+                #compute the dec at time epoch
 		dec = self._dec_0 + (epoch - self._epoch_0) * self._pmdec * self.mas_to_deg
 		
 		return [raCosDec,dec]
 
-		#return self.get_eq_coords(ra,dec)
+	
 
 	def datetime_to_jyTCB(self,date):
 		"""
@@ -156,7 +165,7 @@ class Lens:
 		"""
 		Returns a search box of with defined by the class constant 
 		min_angular_width and with length of the GAIA Mission.
-		This used a a search window for other catalogues to
+		This used as a search window for other catalogues to
 		find sources the lens passes close to.
 
 		Returns:
@@ -173,13 +182,14 @@ class Lens:
 		start = start_end[0]
 		end = start_end[1]
 		
+		#some coordinate geometry in the (ra*cos(dec),dec) plane to get the
+		#the coners of the box
 		hypt = np.sqrt((start[0] - end[0])**(2) + (start[1] - end[1])**(2))
-
 
 		dX = abs((1.0/3600)*self.min_angular_width * (start[1] - end[1]) / hypt)
 		dY = abs((1.0/3600)*self.min_angular_width * (start[0] - end[0]) / hypt)		
                
-		#chech all four possibilities of coordinate arrangements
+		#put the corners of the box in the right place.
 		if ((start[0] < end[0]) and (start[1] < end[1])):
 			return [[start[0] - dX,start[1] + dY],[start[0] + dX,start[1] - dY],[end[0] + dX,end[1] - dY],[end[0] -dX,end[1] + dY]]
 		elif ((start[0] < end[0]) and (start[1] >  end[1])):
@@ -189,31 +199,96 @@ class Lens:
 		else:
 			return [[start[0] + dX,start[1] + dY],[start[0] - dX,start[1] - dY],[end[0] - dX,end[1] - dY],[end[0] + dX,end[1] + dY]]
 	
-	#check if coord is in lens box	
+	
 	def is_coord_in_box(self,ra,dec):
+		"""
+		Checks if a source with coordinates
+		ra, dec is in the search window for the lens.
+
+		Args:
+			ra (double) : Barycentric right ascension
+                                      of a source [Degrees]
+
+			dec (double) :Barycentric declination
+                                      of a source [Degrees]
+
+		Returns:
+			inBox (bool) : Return true if source is in
+				       len's search box, false otherwise.
+		"""
+
 		point = Point(ra*np.cos(np.deg2rad(dec)),dec)
 		box1 = self._lens_box
+		
 		polygon = Polygon([(box1[0][0],box1[0][1]),(box1[1][0],box1[1][1]),(box1[2][0],box1[2][1]),(box1[3][0],box1[3][1])])
+		
 		return polygon.contains(point)
 
 	def get_angular_separation_at_epoch(self,epoch,source_ra,source_dec):
-         	
+         	"""
+		Returns the agular separation between the lens and source
+		at time epoch assuming the source does not move.
+		Implemented using the distance formula in (2.1) in
+		"Candidate Lensing Events for Gaia" doc.
+
+		Args: 
+			epoch (double):  Time at which to find separation epoch 
+					 in Julian year with Barycentric 
+					 coordinate time (TCB)
+			
+			source_ra (double) : Barycentric right ascension of source
+                                             [Degrees] 
+
+			source_dec (double) : Barycentric declination of source
+                                             [Degrees]
+
+		Returns:
+
+			angular_sep (double) : Angular separation of source and 
+					       lens at time = epoch [mas]
+		"""
 		cosSourceDec = np.cos(np.deg2rad(source_dec))	
 		coords = self.get_eq_coords_at_epoch(epoch)
 
 		return (np.sqrt(((source_dec - coords[1])**2) + (((source_ra * cosSourceDec) - (coords[0]))**2))) / self.mas_to_deg 
 		
 	def get_time_of_closest_app(self,source_ra,source_dec):
+		"""
+		Returns the time of closest approach between a 
+		lens and source assuming tha the source does not
+		move. Implemented using formula (1) in 
+		"Candidate Lensing Events for Gaia" doc.
+		
+		Args:
+			source_ra (double) : Barycentric right ascension of source
+                                             [Degrees]
 
+                        source_dec (double) : Barycentric declination of source
+                                             [Degrees]
+
+		Returns:
+	
+			time (double) : time of closest approach [Julian Years]
+
+		"""
+		
+		#convert \mu_{\apha *} from [mas/yr] to [deg/yr]  		
+		#note this term already includes the cos(dec)
 		pmRaDeg = self._pmra * self.mas_to_deg
+		
+		#convert \mu_{\dec *} from [mas/yr] to [deg/yr]
 		pmDecDeg = self._pmdec * self.mas_to_deg
 
+                #pre compute some frequently used quantities 
 		cosSourceDec = np.cos(np.deg2rad(source_dec))
 		cosLensDec = np.cos(np.deg2rad(self._dec_0))
-
+	
+		#calulate time of closest approach from the lens ref epoch
+		#_epoch_0
 		top = - (((pmDecDeg) * (self._dec_0 - source_dec)) + (pmRaDeg) * ((self._ra_0 * cosLensDec) - (source_ra * cosSourceDec)))
 		bottom = (pmDecDeg)**2 + (pmRaDeg)**2
-
+	
+		#return the time of closest approach 
 		return self._epoch_0 + top / bottom 
 
 
